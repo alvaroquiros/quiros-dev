b0f8b0ce8e9802da92c9b709566882508e50a638
Alvaro Quiros <quirosmejias@gmail.com>
2021-09-23 23:10:04 +0200

    Added 3 plugins, added 1 img (testing)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+// @ts-ignore
+import * as styles from './layout.module.css'
 
-const Layout = (props) => (
-    <div>
-      <title>{props.pageTitle}</title>
+{/*This could be done as: Layout = (props) => ... then props.pageTitle*/}
+const Layout = ({pageTitle, children}) => (
+    <div className={styles.container}>
+      <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{props.pageTitle}</h1>
-        {props.children}
+        <ul className={styles.navLinks}>
+            <li className={styles.navLinkItem}>
+              <Link to="/" className={styles.navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={styles.navLinkItem}>
+              <Link to="/about" className={styles.navLinkText}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className={styles.heading}>{pageTitle}</h1>
+          {children}
       </main>
     </div>
 )
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { PageProps } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function AboutPage(props: PageProps) {
   return (
@@ -10,6 +11,9 @@ export default function AboutPage(props: PageProps) {
         </Layout>
 
         <hr/>
+
+        <StaticImage src="../images/foto1.jpeg" alt="Foto avion Miscrosoft Flight Simulator 2020" />
+
         <h2>Path:</h2>
         <p>{props.path}</p>
 
